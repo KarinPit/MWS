b1efe55fdeadbed4848d193cb5f9b6102006e220
KarinPit <99114944+KarinPit@users.noreply.github.com>
2023-11-19 00:05:41 +0200

    made the blog and the project pages an SSR so it wont be static

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,10 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT, 10) || 3000
-  },
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: parseInt(process.env.PORT, 10) || 3000
+  // },
   output: 'server',
   adapter: node({
     mode: "middleware"
