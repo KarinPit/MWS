f061af3ff4e611e02d86383cc249e22eca0e08ef
KarinPit <99114944+KarinPit@users.noreply.github.com>
2024-01-30 19:03:43 +0200

    making the code a little bit more async

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -9,7 +9,6 @@
         <div class="carousel-button">
             <button class="prev" @click="showPrevImage">
                 <img src="/images/arrow-left.svg">
-                <!-- <img src="/images/arrow-left.svg"> -->
             </button>
         </div>
         <div class="carousel-button">
@@ -19,7 +18,7 @@
         </div>
     </div>
 </template>
-    
+  
 <script>
 export default {
     data() {
@@ -33,22 +32,25 @@ export default {
         };
     },
     async created() {
-        this.urls = await GetHomeImages();
+        try {
+            this.urls = await GetHomeImages();
+        } catch (error) {
+            console.error('Error fetching images:', error);
+            // Handle the error appropriately
+        }
     },
     methods: {
-        async showPrevImage() {
+        showPrevImage() {
             if (this.currentIndex > 0) {
                 this.currentIndex--;
-            }
-            else {
+            } else {
                 this.currentIndex = this.urls.imagesUrl.length - 1;
             }
         },
-        async showNextImage() {
+        showNextImage() {
             if (this.currentIndex < this.urls.imagesUrl.length - 1) {
                 this.currentIndex++;
-            }
-            else {
+            } else {
                 this.currentIndex = 0;
             }
         },
@@ -64,20 +66,23 @@ export default {
 };
 
 async function GetHomeImages() {
-    const response = await fetch("https://mws-data-280b2464bf34.herokuapp.com/api/home-image?populate=*");
-    const { data } = await response.json();
-    const mainImageUrl = data.attributes.mainImage.data.attributes.url;
-    const images = data.attributes.images.data;
-    const imagesUrl = images.map((image) => image.attributes.url);
-    const urls = {
-        mainImageUrl: mainImageUrl,
-        imagesUrl: imagesUrl,
-    };
-    return urls;
+    try {
+        const response = await fetch("https://mws-data-280b2464bf34.herokuapp.com/api/home-image?populate=*");
+        const { data } = await response.json();
+        const mainImageUrl = data.attributes.mainImage.data.attributes.url;
+        const images = data.attributes.images.data;
+        const imagesUrl = images.map((image) => image.attributes.url);
+        return {
+            mainImageUrl: mainImageUrl,
+            imagesUrl: imagesUrl,
+        };
+    } catch (error) {
+        console.error('Error fetching images:', error);
+        throw error; // Re-throw the error for further handling
+    }
 }
 </script>
-
-
+  
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
